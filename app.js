8736154499f74c426196527efa2b458f077b314a
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2021-10-13 21:25:45 -0400

    done

---
+++ app.js
@@ -0,0 +1,50 @@
+//Time Complexity: O(n)
+function logUpTo(n) {
+    for (let i = 1; i <= n; i++) {
+        console.log(i);
+    }
+}
+
+//Time Complexity: O(n)
+function logAtLeast10(n) {
+    for (let i = 1; i <= Math.max(n, 10); i++) {
+        console.log(i);
+    }
+}
+
+//Time Complexity: O(n)
+function logAtMost10(n) {
+    for (let i = 1; i <= Math.min(n, 10); i++) {
+        console.log(i);
+    }
+}
+
+//Time Complexity: O(n^2)
+function subtotals(array) {
+    let subtotalArray = [];
+    for (let i = 0; i < array.length; i++) {
+        let subtotal = 0;
+        for (let j = 0; j <= i; j++) {
+            subtotal += array[j];
+        }
+        subtotalArray.push(subtotal);
+    }
+    return subtotalArray;
+}
+//Time Complexity: O(n)
+function vowelCount(str) {
+    let vowelCount = {};
+    const vowels = "aeiouAEIOU";
+
+    for (let char of str) {
+        if (vowels.includes(char)) {
+            if (char in vowelCount) {
+                vowelCount[char] += 1;
+            } else {
+                vowelCount[char] = 1;
+            }
+        }
+    }
+
+    return vowelCount;
+}
---
+++ short.md
@@ -0,0 +1,15 @@
+# Part 3 - Short Answer
+
+1. True or false: n^2 + n is O(n^2). True
+1. True or false: n^2 * n is O(n^3). True
+1. True or false: n^2 + n is O(n). False
+1. What’s the time complexity of the .indexOf array method? O(n)
+1. What’s the time complexity of the .includes array method? O(n)
+1. What’s the time complexity of the .forEach array method? O(n) at least (depends on what the callback does)
+1. What’s the time complexity of the .sort array method? O(n log n)
+1. What’s the time complexity of the .unshift array method? O(n)
+1. What’s the time complexity of the .push array method? O(1)
+1. What’s the time complexity of the .splice array method? O(n) it can be O(1) if the end, but we can’t assume that
+1. What’s the time complexity of the .pop array method? O(1)
+1. What’s the time complexity of the Object.keys() function? O(n)
+1. What’s the space complexity of the Object.keys() function? O(n)
